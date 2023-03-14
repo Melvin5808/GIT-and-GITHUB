6b021fbb88f8876252bc538fd3ab733961016ed7
Melvin5808 <127872703+Melvin5808@users.noreply.github.com>
2023-03-14 16:41:11 -0400

    Create hello_goodbye.py

---
+++ hello_goodbye.py
@@ -0,0 +1,13 @@
+"""INST326 Exercise 4 - Saying hello and goodbye using Git
+Driver: Melvin Vasquez-Gomez
+Navigator: 
+March 14, 2023
+
+"""
+
+def hello():
+    print("hello")
+
+
+def goodbye():
+    print("goodbye")
