f87ed32d712c17a7288e449a5daa9e4cd3f63da6
Lorenzo3281 <lorenzoa@terpmail.umd.edu>
2023-03-16 16:53:25 -0400

    updated docstrings
    added attributes and side effects to docstrings

--- hello_goodbye.py
+++ hello_goodbye.py
@@ -6,6 +6,8 @@ March 14, 2023
 
 def hello():
     """ Prints hello
+    Side Effects: prints hello
+    Attributes: there are no attributes
     """
 
     print("hello")
@@ -13,6 +15,8 @@ def hello():
 
 def goodbye():
     """Prints goodbye
+    Side Effects: prints goodbye
+    Attributes: there are no attributes
     """
 
     print("goodbye")
